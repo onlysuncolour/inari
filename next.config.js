c6775d3c4168628fc6f3459d4599084e06e15250
Young.liu <onlysuncolour@sina.com>
2023-03-24 22:01:11 +0800

    feat: add less & change main

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const withLess = require("next-with-less");
+
 const nextConfig = {
   reactStrictMode: true,
 }
-
-module.exports = nextConfig
+module.exports = withLess({
+  lessLoaderOptions: {
+    /* ... */
+  },
+  ...nextConfig
+});
